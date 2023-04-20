15778fcc16e26a96713304eacd586820b57609ce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-20 12:00:13 +0300

    Created the styled about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,22 @@
+import { Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {}
 
 const about = (props: Props) => {
-	return <div>about</div>
+	return (
+		<Flex
+			align={'center'}
+			justify={'center'}
+			height={'100vh'}
+			width={'100vw'}
+			overflow={'hidden'}
+		>
+			<Text bg={'slateblue.500'} p='6rem' fontSize={'4rem'}>
+				About
+			</Text>
+		</Flex>
+	)
 }
 
 export default about
