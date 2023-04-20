b2351967020fbe92dd0b046467214a0f8005734f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-20 11:34:30 +0300

    Added a bunch of lorem ipsum text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,32 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 	return (
 		<div>
-			<Text as={'section'}>This is a text component</Text>
+			<Text as={'section'}>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+				Commodi eaque rem praesentium unde nobis dolorem dolor sit
+				similique provident. Totam ducimus deleniti rerum reiciendis a,
+				saepe adipisci aspernatur illo voluptate reprehenderit cum
+				expedita ullam officiis minus, alias sapiente? Nulla maiores,
+				nesciunt sequi porro error nam mollitia nostrum neque, officiis
+				laboriosam blanditiis, dignissimos numquam quia ea quaerat
+				facere maxime eum ipsam. Perspiciatis esse quas illum ea nam
+				iusto, enim minus minima, corporis, in non excepturi a. Illum
+				dignissimos aliquid delectus obcaecati, similique quia dolores
+				non libero ea quo pariatur deleniti doloremque veniam cum
+				deserunt iste sint! Similique dignissimos possimus quaerat vero
+				nesciunt facere voluptas repudiandae illum dicta illo dolore,
+				aperiam quidem, vitae tempora ad. Esse nihil, ipsum corrupti ex
+				ab cum distinctio reiciendis suscipit ut assumenda sed deleniti
+				at et minima sapiente unde atque alias! Corrupti ipsa at nisi
+				non reprehenderit minima iure nobis, molestiae dolor qui ea
+				corporis aperiam voluptas distinctio deleniti, est blanditiis ad
+				tenetur reiciendis sunt! Reprehenderit eaque, corporis repellat
+				corrupti quae dignissimos temporibus magni sequi eligendi! Quae
+				quibusdam vero aut maiores soluta magni? Rem architecto dolorum
+				distinctio amet voluptates possimus in repellat vero odio
+				dignissimos! Ducimus est sequi fuga atque facere sint iusto
+				harum eum at maiores?
+			</Text>
 		</div>
 	)
 }
