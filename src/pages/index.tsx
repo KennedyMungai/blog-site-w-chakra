8c447b61cef448fffc81760989841ed5fb19a40f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-20 11:31:29 +0300

    Added a chakra ui button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@chakra-ui/react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-	return <></>
+	return <Button>Chicken Shit</Button>
 }
